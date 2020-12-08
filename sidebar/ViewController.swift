de6161aac3372573e9cdbb15a3e2b4fd34a2f0f6
Alex Logan <me@alexanderlogan.co.uk>
2020-12-08 16:38:40 +0000

    Initial Commit

---
+++ sidebar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sidebar
+//
+//  Created by Alex Logan on 08/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
